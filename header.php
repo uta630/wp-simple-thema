237f3fdd556f63614c8d4d4e77fb772085faf432
Usui Takeshi <usuitkc.630@gmail.com>
2020-02-05 18:33:51 +0900

    <head>:all in one seo packで設定するので諸々修正

--- header.php
+++ header.php
@@ -2,12 +2,9 @@
 <html lang="ja">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('name'); ?></title>
     <meta name="robots" content="index, follow">
-    <meta name="Description" content="ワードプレス初学者サイト">
-    <meta name="copyright" content="(c)copyright area 2018">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <link rel="shortcut icon" href="./screenshot.png" type="image/x-icon">
-    <?php wp_head(); ?>
 </head>
