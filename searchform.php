b5f0ca14d3a3205621fcf197831b31d58d36fc1e
Usui Takeshi <usuitkc.630@gmail.com>
2020-02-05 17:54:15 +0900

    Audits:検索窓

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" class="c-search" action="<?php echo esc_url( home_url('/') ); ?>">
-    <input type="search" name="s" class="c-search__field" value="<?php echo get_search_query(); ?>">
-    <button class="c-search__submit material-icons"></button>
+    <label for="search" class="c-search__label">検索</label><input type="search" name="s" class="c-search__field" id="search" value="<?php echo get_search_query(); ?>">
+    <button class="c-search__submit">検索</button>
 </form>
