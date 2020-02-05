907d212b9d302dfef3f792e57a10d2f938ea408d
Usui Takeshi <usuitkc.630@gmail.com>
2020-02-05 11:55:22 +0900

    404ページ反映

--- 404.php
+++ 404.php
@@ -1,24 +1,21 @@
 <?php get_header(); ?>
 
-    <body <?php body_class(); ?>>
-        <?php get_template_part('content', 'menu'); ?>
+<body <?php body_class(); ?>>
 
-        <main class="main main-col" ontouchstart="">
-            <div class="main--inner">
-                <section class="block" id="coding">
-                    <div class="block-col post">
-                        <div class="post__text">
-                            <h2>お探しのページが見つかりませんでした。</h2>
+<?php get_template_part('content', 'menu'); ?>
 
-                            <p>申し訳ありません。お探しのページが見つかりませんでした。</p>
-                            <p>お手数ですが改めてお求めのページをお探しください。</p>
-                            <a href="<?php echo home_url() ?>">TOPページに戻る</a>
-                        </div>
-                    </div>
-                </section>
-                
-                <?php get_sidebar(); ?>
-            </div>
-        </main><!-- /main -->
+<div class="c-section l-section">
+    <main class="c-section__primary">
+        <h1 class="c-section__heading">お探しのページが見つかりませんでした。</h1>
+
+        <div class="c-error">
+            <p>お探しのページが見つかりませんでした。</p>
+            <p>お手数ですが改めてお求めのページをお探しください。</p>
+            <a href="<?php echo home_url() ?>">TOPページへ</a>
+        </div>
+    </main>
+
+	<?php get_sidebar(); ?>
+</div>
 
 <?php get_footer(); ?>
